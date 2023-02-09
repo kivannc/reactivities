2b5b3003b79a61c4d3d02044939f86cb64c591b9
Kivanc Erturk <erturkkivanc@gmail.com>
2023-02-09 21:36:21 +0300

    isSubmitting Added

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -31,9 +31,9 @@ const requests = {
 const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
     details: (id : string ) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity : Activity) => requests.post('/activities', activity),
-    update: (activity : Activity) => requests.put(`/activities/${activity.id}`, activity),
-    delete: (id : string ) => requests.del(`/activities/${id}`)
+    create: (activity : Activity) => requests.post<void>('/activities', activity),
+    update: (activity : Activity) => requests.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id : string ) => requests.del<void>(`/activities/${id}`)
 }
 
 const agent = {
