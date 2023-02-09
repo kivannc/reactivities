9de0ae22073198a328941a70156137b61592a1ec
Kivanc Erturk <erturkkivanc@gmail.com>
2023-02-09 18:04:46 +0300

    agen.ts created

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosResponse } from "axios";
+import { Activity } from "../models/activity";
+
+axios.defaults.baseURL = "http://localhost:5000/api";
+
+const responseBody = (response : AxiosResponse ) => response.data; 
+
+const requests = {
+    get: (url : string ) => axios.get(url).then(responseBody),
+    post: (url : string , body : {}) => axios.post(url, body).then(responseBody),
+    put: (url : string , body : {}) => axios.put(url, body).then(responseBody),
+    del: (url : string ) => axios.delete(url).then(responseBody)
+}
+
+const Activities = {
+    list: () => requests.get('/activities'),
+    details: (id : string ) => requests.get(`/activities/${id}`),
+    create: (activity : Activity) => requests.post('/activities', activity),
+    update: (activity : Activity) => requests.put(`/activities/${activity.id}`, activity),
+    delete: (id : string ) => requests.del(`/activities/${id}`)
+}
+
+const agent = {
+    Activities
+}
+
+export default agent;
