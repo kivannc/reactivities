2599efcd49eeab013b5d289903c975656ac05292
Kivanc Erturk <erturkkivanc@gmail.com>
2023-02-12 22:02:36 +0300

    Status code changes implemented

---
+++ Application/Core/Result.cs
@@ -0,0 +1,16 @@
+namespace Application.Core;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; set; }
+    public T Value { get; set; }
+
+    public string Error { get; set; }
+
+    public static Result<T> Success(T Value) =>
+        new Result<T> { IsSuccess = true, Value = Value };
+
+    public static Result<T> Failure(string Error) =>
+        new Result<T> { IsSuccess = false, Error = Error };
+}
+
