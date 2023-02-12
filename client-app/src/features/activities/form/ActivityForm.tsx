643bdad0b21d946e3c9d4af0850d6ea7ddd5cf81
Kivanc Erturk <erturkkivanc@gmail.com>
2023-02-12 14:27:23 +0300

    form submission navigate

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -2,12 +2,14 @@ import { observer } from "mobx-react-lite";
 import { ChangeEvent, useEffect, useState } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { v4 as uuid } from "uuid";
 
 export default observer(function ActivityForm() {
   const { activityStore } = useStore();
   const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
 
   const [activity, setActivity] = useState({
     id: "",
@@ -32,7 +34,17 @@ export default observer(function ActivityForm() {
   }, [id, loadActivity]);
 
   function handleSubmit() {
-    activity?.id ? updateActivity(activity) : createActivity(activity);
+    if (!activity.id) {
+      activity.id = uuid();
+      createActivity(activity).then(() =>
+        navigate(`/activities/${activity.id}`)
+      );
+    }
+    else {
+      updateActivity(activity).then(() =>
+        navigate(`/activities/${activity.id}`)
+      );
+    }
   }
 
   function handleInputChange(
