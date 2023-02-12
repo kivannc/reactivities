419a565ade79105320e79a8f2be21f435b6b4433
Kivanc Erturk <erturkkivanc@gmail.com>
2023-02-12 22:31:36 +0300

    error testing code added. Toast added

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,43 +1,62 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
 import { Activity } from "../models/activity";
 
-const sleep = (delay : number ) => {
-    return new Promise((resolve) => {
-        setTimeout(resolve, delay);
-    })
-}
+const sleep = (delay: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+};
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
-axios.interceptors.response.use(async response => {
-    try {
-        await sleep(1000);
-        return response;
-    } catch (error) {
-        console.log(error);
-        return await Promise.reject(error);
+axios.interceptors.response.use(
+  async (response) => {
+    await sleep(1000);
+    return response;
+  },
+  (error: AxiosError) => {
+    const { data, status, config } = error.response!;
+    switch (status) {
+      case 400:
+        toast.error("Bad request");
+        break;
+      case 401:
+        toast.error("Unauthorized");
+        break;
+      case 404:
+        //navigate("/notfound");
+        toast.error("Not found");
+        break;
+      case 500:
+        toast.error("Server error");
+        break;
     }
-})
-    
-const responseBody =<T> (response : AxiosResponse<T> ) => response.data; 
+    return Promise.reject(error);
+  }
+);
+
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
-    get:<T> (url : string ) => axios.get<T>(url).then(responseBody),
-    post:<T> (url : string , body : {}) => axios.post<T>(url, body).then(responseBody),
-    put:<T> (url : string , body : {}) => axios.put<T>(url, body).then(responseBody),
-    del:<T> (url : string ) => axios.delete<T>(url).then(responseBody)
-}
+  get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+  post: <T>(url: string, body: {}) =>
+    axios.post<T>(url, body).then(responseBody),
+  put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+  del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+};
 
 const Activities = {
-    list: () => requests.get<Activity[]>('/activities'),
-    details: (id : string ) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity : Activity) => requests.post<void>('/activities', activity),
-    update: (activity : Activity) => requests.put<void>(`/activities/${activity.id}`, activity),
-    delete: (id : string ) => requests.del<void>(`/activities/${id}`)
-}
+  list: () => requests.get<Activity[]>("/activities"),
+  details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+  create: (activity: Activity) => requests.post<void>("/activities", activity),
+  update: (activity: Activity) =>
+    requests.put<void>(`/activities/${activity.id}`, activity),
+  delete: (id: string) => requests.del<void>(`/activities/${id}`),
+};
 
 const agent = {
-    Activities
-}
+  Activities,
+};
 
 export default agent;
