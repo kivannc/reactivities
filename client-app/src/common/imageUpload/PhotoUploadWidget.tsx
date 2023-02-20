77191bb9a048fcd5adacfbe54ba0a27d3c6d2ed3
Kivanc Erturk <erturkkivanc@gmail.com>
2023-02-20 23:35:07 +0300

    ImageUpload dropzone added

--- client-app/src/common/imageUpload/PhotoUploadWidget.tsx
+++ client-app/src/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,14 +1,20 @@
-import { Grid, Header } from "semantic-ui-react";
+import { useState } from "react";
+import { Grid, Header, Image } from "semantic-ui-react";
+import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
 
 export default function PhotoUploadWidget() {
+  const [files, setFiles] = useState<any>([]);
+
   return (
     <Grid>
       <Grid.Column width={4}>
         <Header color="teal" sub content="Step 1 - Add Photo" />
+        <PhotoWidgetDropzone setFiles={setFiles} />
       </Grid.Column>
       <Grid.Column width={1} />
       <Grid.Column width={4}>
         <Header color="teal" sub content="Step 2 - Resize Photo" />
+        {files && files.length > 0 && <Image src={files[0].preview} />}
       </Grid.Column>
       <Grid.Column width={1} />
       <Grid.Column width={4}>
